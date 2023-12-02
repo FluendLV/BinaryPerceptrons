fbbb170d5db39a99db6ed20cc09fbf8d7fda16a0
FluendLV <mahito12345@gmail.com>
2023-12-02 10:49:56 +0200

    multilayer

--- IMNT02.py
+++ IMNT02.py
@@ -1,124 +1,59 @@
-import numpy as np
 import pandas as pd
-from math import exp
+import tensorflow as tf
+import numpy as np
+from sklearn.model_selection import train_test_split
+import random
 
-# Load data from CSV file into a DataFrame
-df = pd.read_csv("IMNT_DATASET.csv")
-# Extracting the 'Tasks', 'Bugs', and 'Issues' columns as feature attributes
-attributes = df[['Tasks', 'Bugs', 'Issues']].values
-# Extracting the 'Class' column as class labels
-classes = df['Class'].values
-# Getting unique class values
-unique_classes = np.unique(classes)
+# Read the dataset from 'IMNT_DATASET.csv'
+dati_df = pd.read_csv('IMNT_DATASET.csv')
 
-# Normalize the attributes
-# Subtracting mean (axis=0 ensures the mean of each column is taken) 
-# and dividing by standard deviation for each column respectively
-normalized_attributes = (attributes - attributes.mean(axis=0)) / attributes.std(axis=0)
+# Extract the target variable and one-hot encode it
+gaidamas_vertibas = pd.get_dummies(dati_df['Class'], dtype=float)
 
-# Shuffling data and splitting it into training and testing datasets
-indices = np.arange(len(attributes))
-np.random.shuffle(indices)
+# Split the dataset into training and testing sets
+ieejas_train, ieejas_test, gaidamas_train, gaidamas_test = train_test_split(
+    dati_df.drop(columns=['Class']),  # Remove the 'Class' column
+    gaidamas_vertibas,
+    test_size=0.3,
+    stratify=gaidamas_vertibas
+)
 
-train_size = int(0.8 * len(attributes))
-train_indices = indices[:train_size]
-test_indices = indices[train_size:]
+# Calculate means and standard deviations for normalization
+means = ieejas_train.mean()
+stds = ieejas_train.std()
 
-train_attributes = normalized_attributes[train_indices]
-train_classes = classes[train_indices]
+# Normalize the input features
+ieejas_train = (ieejas_train - means) / stds
+ieejas_test = (ieejas_test - means) / stds
 
-test_attributes = normalized_attributes[test_indices]
-test_classes = classes[test_indices]
+# Convert data to NumPy arrays
+ieejas_train = ieejas_train.to_numpy()
+ieejas_test = ieejas_test.to_numpy()
+gaidamas_train = gaidamas_train.to_numpy()
+gaidamas_test = gaidamas_test.to_numpy()
 
-# Defining the perceptron model
-lr = 0.2
+# Set random seeds for reproducibility
+SEED = 123
+random.seed(SEED)
+tf.random.set_seed(SEED)
+np.random.seed(SEED)
 
-class Perceptron:
-    def __init__(self, input_size: int):
-        self.w = np.zeros(input_size)  # Initialize weights
-        self.w0 = 0  # Bias term
-        self.grad = 0  # Gradient
-    
-    def predict(self, input_values):
-        # Calculate the weighted sum
-        net = np.dot(input_values, self.w) + self.w0
-        # Calculate the gradient using derivative of sigmoid
-        self.grad = self.der_sigmoid(net)
-        # Return the predicted value (between 0 and 1)
-        return self.act_sigmoid(net)
+# Initialize the model
+initializer = tf.keras.initializers.RandomUniform(minval=0., maxval=1., seed=SEED)
 
-    def fit(self, input_values, output_value):
-        # Predict the output for given input values
-        y = self.predict(input_values)
-        # Calculate the error
-        delta = output_value - y
-        if delta != 0:
-            # Adjust weights and bias based on the error
-            for i in range(len(self.w)):
-                self.w[i] += lr * delta * self.grad * input_values[i]
-                self.w0 += lr * delta * self.grad
+model = tf.keras.models.Sequential([
+    tf.keras.layers.Dense(3, activation='linear', kernel_initializer=initializer, bias_initializer=initializer, input_shape=(ieejas_train.shape[1],)),
+    tf.keras.layers.Dense(2, activation='linear', kernel_initializer=initializer, bias_initializer=initializer),
+    tf.keras.layers.Dense(gaidamas_vertibas.shape[1], activation='softmax', kernel_initializer=initializer, bias_initializer=initializer)
+])
 
-    # Activation function: Sigmoid
-    def act_sigmoid(self, val):
-        return 1.0 / (1.0 + exp(-val))
+# Compile the model
+model.compile(optimizer='Adam', loss='categorical_crossentropy', metrics=['accuracy'])
 
-    # Derivative of the sigmoid function
-    def der_sigmoid(self, val):
-        s = self.act_sigmoid(val)
-        return s * (1 - s)
-    
+# Train the model
+history = model.fit(ieejas_train, gaidamas_train, epochs=250)
 
-# Training perceptrons for each unique class
-perceptrons = [Perceptron(3) for _ in unique_classes]
-for epoch in range(1000):
-    for di in range(len(train_attributes)):
-        target_class = train_classes[di]
-        for i, uc in enumerate(unique_classes):
-            expected_output = 1 if target_class == uc else 0
-            perceptrons[i].fit(train_attributes[di], expected_output)
-
-# Testing perceptrons and printing results
-for di in range(len(test_attributes)):
-    outputs = [perc.predict(test_attributes[di]) for perc in perceptrons]
-    predicted_class = unique_classes[np.argmax(outputs)]
-    original_data = attributes[test_indices[di]]
-    print(f"Object: {original_data}, Real class: {test_classes[di]}, Predicted class: {predicted_class}")
-
-# Function to calculate RMSE and number of misclassifications
-def calculate_errors(attributes, classes, perceptrons):
-    sum_squared_errors = 0
-    misclassification_count = 0
-    for di in range(len(attributes)):
-        outputs = [perc.predict(attributes[di]) for perc in perceptrons]
-        predicted_class = unique_classes[np.argmax(outputs)]
-        actual_class = classes[di]
-        # Sum squared errors for RMSE
-        sum_squared_errors += (predicted_class - actual_class)**2
-        # Count misclassifications
-        if predicted_class != actual_class:
-            misclassification_count += 1
-    # Calculate RMSE
-    rmse = np.sqrt(sum_squared_errors / len(attributes))
-    return rmse, misclassification_count
-
-# Calculating and printing RMSE and number of misclassifications for training and testing datasets
-train_rmse, train_misclassified_count = calculate_errors(train_attributes, train_classes, perceptrons)
-test_rmse, test_misclassified_count = calculate_errors(test_attributes, test_classes, perceptrons)
-
-print("\nTraining Data Metrics:")
-print(f"RMSE: {train_rmse:.3f}")
-print(f"Misclassified: {train_misclassified_count} out of {len(train_attributes)}")
-
-print("\nTesting Data Metrics:")
-print(f"RMSE: {test_rmse:.3f}")
-print(f"Misclassified: {test_misclassified_count} out of {len(test_attributes)}")
-
-# Making a prediction for a new data point
-new_data = np.array([2, 3, 2])
-# Normalize the new data using the mean and standard deviation of the original dataset
-normalized_new_data = (new_data - attributes.mean(axis=0)) / attributes.std(axis=0)
-# Predict the class for the new data point
-outputs = [perc.predict(normalized_new_data) for perc in perceptrons]
-predicted_class = unique_classes[np.argmax(outputs)]
-
-print(f"For input {new_data}, predicted class is {predicted_class}")
+# Evaluate the model on the test s 
+result = model.evaluate(ieejas_test, gaidamas_test)
+print("Test loss:", result[0])
+print("Test accuracy:", result[1])
